0f8899833fb56875341fc58c0faeb29adc8a1bcc
mwickramanayaka <manul.wickramanayaka@insighture.com>
2021-05-19 20:17:34 +0530

    method declaration
    setBrowser();
    setBrowserConfig();
    runTest();

--- seleniumTest/src/seleniumTest/seleniumTest.java
+++ seleniumTest/src/seleniumTest/seleniumTest.java
@@ -4,50 +4,65 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class seleniumTest {
 
+	static String browser = "";
+	static WebDriver driver;
+
 	public static void main(String[] args) throws InterruptedException {
-		
-		System.setProperty("webdriver.chrome.driver", "./chromeDriver/chromedriver.exe");
+
+		setBrowser();
+		setBrowserConfig();
+		runTest();
+
+	}
+	public static void setBrowser() {
+
+		browser = "chrome";
+
+	}
+
+	public static void setBrowserConfig() {
+
+		if (browser.contains("fireefox")) {
+			System.setProperty("webdriver.chrome.driver", "./chromeDriver/geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+
+		if (browser.contains("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "./chromeDriver/chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+	}
+
+	public static void runTest() throws InterruptedException {
+
 		String baseURL = "https://apply.sliit.lk/DownloadApplication/Inquiry";
-		
-		WebDriver driver =new ChromeDriver();
+
 		driver.manage().window().maximize();
 		driver.get(baseURL);
-		
+
 		WebElement checkBox = driver.findElement(By.xpath("//*[@id=\"ProgramFields\"]/div/div/label[1]"));
 		checkBox.click();
-		
+
 		Thread.sleep(2000);
-		
+
 		if (checkBox.isSelected()) {
 			System.out.println("check box selected");
 		}
-		
+
 		WebElement CountryDropDown = driver.findElement(By.xpath("//*[@id=\"country\"]"));
 		Select Country = new Select(CountryDropDown);
-		
+
 		Country.selectByVisibleText("Syria");
 		Thread.sleep(2000);
-		
+
 		driver.quit();
 
-		/*
-		WebElement SearchField = driver.findElement(By.name("q"));
-		SearchField.sendKeys("Chrome");
-		
-		Thread.sleep(5000);
-
-		WebElement SearchButton = driver.findElement(By.name("btnK"));
-		SearchButton.submit(); 
-		
-		Thread.sleep(3000);
-		 
-		driver.quit();
-		*/
-		
 	}
-
 }
+
+
